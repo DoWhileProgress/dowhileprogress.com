7569e0145aac40c7cd2ee3a3cc08850da2368dd2
Trevor Suarez <rican7@gmail.com>
2023-09-01 14:22:45 -0600

    Improving positioning of the minimize animation

--- scripts/main.js
+++ scripts/main.js
@@ -3,12 +3,32 @@
 
   console.log('do { } while (progress);');
 
+  const documentElement = document.documentElement;
   const guiWindowElement = document.querySelector('.gui-window');
   const guiWindowIconElement = document.querySelector('.gui-window-icon');
   const guiWindowMinimizeElement = document.querySelector('.gui-window-control-minimize');
   const guiWindowMaximizeElement = document.querySelector('.gui-window-control-maximize');
   const guiWindowCloseElement = document.querySelector('.gui-window-control-close');
   const copyrightYearElement = document.querySelector('#copyright-year');
+
+  const debounce = (func, time) => {
+    time ??= 100;
+    let timer;
+
+    return (event) => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+
+      timer = setTimeout(func, time, event);
+    };
+  };
+
+  const markCriticalDimensions = () => {
+    documentElement.style.setProperty('--document-height', documentElement.clientHeight + 'px');
+    guiWindowElement.style.setProperty('--gui-window-top', guiWindowElement.offsetTop + 'px');
+    guiWindowElement.style.setProperty('--gui-window-height', guiWindowElement.offsetHeight + 'px');
+  };
 
   const recursivelyRestartAnimations = (element) => {
     const animationName = window.getComputedStyle(element).animationName;
@@ -23,6 +43,10 @@
       recursivelyRestartAnimations(child);
     }
   };
+
+  // Mark critical dimensions as CSS vars, and continue to do so on resize
+  markCriticalDimensions();
+  window.addEventListener('resize', debounce(markCriticalDimensions));
 
   guiWindowIconElement.addEventListener('dblclick', () => {
     recursivelyRestartAnimations(guiWindowElement);
